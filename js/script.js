6b63400aff0a09a3ab16d8b3951951e66fbc2b64
theniloywebdev <husnemobarakniloy2001@gmail.com>
2023-10-05 16:04:35 +0600

    tailwindcss

---
+++ js/script.js
@@ -0,0 +1,4 @@
+<div class="">
+<i class="fa-solid fa-circle-chevron-left"></i>
+<i class="fa-solid fa-circle-chevron-right"></i>
+</div>
---
+++ js/tailwind config.js
@@ -0,0 +1,61 @@
+tailwind.config = {
+    theme: {
+      extend: {
+
+        // COLOR PALLATE START 
+        colors: {
+          headerBgColor: '#1C1E53',
+          headerTColor: '#BBBBCB',
+          headerTHColor: '#FFF',
+          headerBColor: '#FEF6FF',
+          bannerBgColor: '#1C1E53',
+          bannerBTColor: '#1B1C2B',
+          bannerBbgColor: '#FCD980',
+          workBgColor: '#F4F6FC',
+          workHColor: '#282938',
+          workPcolor: 'rgb(40, 41, 56, 0.7)',
+          workANTColor: '#2405F2',
+          featureIcolor: '#2405F2',
+          clientBgcolor: '#F7F9FF',
+          contactOVcolor: 'rgba(28, 30, 83, 0.5)',
+          contactAntBgcolor: '#1C1E53',
+          contactPcolor: '#F4F6FC',
+          footerPcolor: 'rgba(255, 255, 255, 0.8)',
+          footBgcolor: '#FCD980',
+          footerAntPcolor: 'rgba(40, 41, 56, 0.8)',
+          questionPcolor: 'rgba(37, 5, 242, 0.87)',
+          cardOcolor: 'rgba(28, 30, 83, 0.8)',
+          cardOAcolor: 'rgba(28, 30, 83, 0.24)',
+         
+         
+        },
+        // COLOR PALLATE START 
+
+        // CONTAINER WIDTH PART START
+        width: {
+          headerContainer: "1280px",
+          workContainer: "1170px",
+          footerContainer: "1060px",
+        },
+        
+        // CONTAINER WIDTH PART END
+
+        // FONT FAMILY START 
+        fontFamily: {
+          poppinsF: ["'Poppins', sans-serif;"],
+          manropeF: ["'Manrope', sans-serif;"],
+        },
+        // FONT FAMILY END
+
+        // BACKGROUND PART START 
+        backgroundImage: {
+          contactBgImg: "url('images/contact_bg.png')",
+          projectCOneBgImg: "url('images/project_card_one.png')",
+          projectCTwoBgImg: "url('images/project_card_two.png')",
+          projectCThreeBgImg: "url('images/project_card_three.png')",
+        }
+        // BACKGROUND PART END
+        
+      }
+    }
+  }
